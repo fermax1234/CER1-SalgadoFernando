3ca0c7af3ac5548cb6214f1010daf7ed12f6dd71
fermax1234 <39860869+fermax1234@users.noreply.github.com>
2024-04-25 19:20:36 -0400

    Add files via upload

---
+++ js/jsFer.js
@@ -0,0 +1,38 @@
+function cambiarPrincipal1(){
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/1th.jpg");
+    
+}
+function cambiarPrincipal2(){
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/2th.jpg");
+    
+}
+function cambiarPrincipal3(){
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/3th.jpg");
+    
+}
+function cambiarPrincipal4(){
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/4th.jpg");
+    
+}
+function cambiarPrincipal5(){
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/5th.jpg");
+    
+ }
+function cambiarPrincipal6(){
+    
+    var grande= document.getElementById("imgGrandecita");
+
+    grande.setAttribute('src',"img/6th.jpg");
+    
+    
+}
